aca654670adc5ae72539c1269c653b6abddf844d
alfonsojk <alfonso.juarezkelly@gmail.com>
2020-11-18 19:23:16 -0600

    first commit

---
+++ header.php
@@ -0,0 +1,2 @@
+<link rel="icon" href="https://www.wpbeginner.com/favicon.png" type="image/x-icon" />
+<link rel="shortcut icon" href="https://www.wpbeginner.com/favicon.png" type="image/x-icon" />
---
+++ index.php
@@ -0,0 +1,158 @@
+<html>
+<?php get_header(); ?>
+	<head>
+		<title>Alfonso JK</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/assets/css/main.css" />
+	</head>
+	<body class="is-preload">
+
+		<!-- Header -->
+			<header id="header">
+				<div class="inner">
+					<a href="#" class="image avatar"><img src="<?php echo get_stylesheet_directory_uri()?>/images/avatar.jpg" alt="" /></a>
+					<h1><strong>HEY, I'M ALFONSO</strong>
+				</div>
+				<div class="inner">
+					<ul class="icons">
+						<li><a href="#" class="icon solid fa-envelope"><span class="label">Email</span></a></li>
+						<li><a href="#" class="icon brands fa-github"><span class="label">Github</span></a></li>
+						<li><a href="#" class="icon brands fa-linkedin"><span class="label">Linkedin</span></a></li>
+						<li><a href="#" class="icon brands fa-free-code-camp"><span class="label">Dribbble</span></a></li>
+					</ul>
+				</div>
+			</header>
+
+		<!-- Main -->
+			<div id="main">
+
+				<!-- One -->
+					<section id="one">
+						<header class="major">
+							<h2 style="margin-bottom:10%;text-decoration:overline;display: flex;justify-content: center;">W E B _ D E V E  L O P E R <br />
+							</h2>
+						</header>
+						<p style= "display: flex;justify-content: center;">WEB DEVELOPER AND DESIGNER FROM MEXICO. I ENJOY CODING, DESING AND BUILDING LAYOUTS FOR WEBSITES TO HELP PEOPLE GO FURTHER ONLINE.</p>
+						<ul class="actions" style= "display: flex;justify-content: center;">
+							<li><a href="#" class="button" style="text-decoration:overline0;" >projects</a></li>
+						</ul>
+					</section>
+
+				<!-- Two -->
+					<section id="two" style="margin-top: 35%;">
+						<h2 style= "text-decoration:overline;">technical</h2>
+						<div class="container">
+    <!-- Example row of columns -->
+    <div class="row">
+      <div class="col-md-4">
+        <h2 style="text-decoration:underline;">Desing</h2>
+        <p>I value simple content structure, clean design patterns, and thoughtful interactions. </p>
+        <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+      </div>
+      <div class="col-md-4">
+        <h2 style="text-decoration:underline;">Front End Developer</h2>
+        <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+        <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+      </div>
+      <div class="col-md-4">
+        <h2 style="text-decoration:underline;"0>Back End Developer</h2>
+        <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+        <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+      </div>
+    </div>
+						                                            
+						                        
+					</section>
+
+					<!-- Two -->
+					<section id="two" style="margin-top: 35%;">
+						<h2 style="text-decoration:overline;">projects</h2>
+						<div class="row">
+							<article class="col-6 col-12-xsmall work-item">
+								<a href="images/fulls/01.jpg" class="image fit thumb"><img src="<?php echo get_stylesheet_directory_uri()?>/images/thumbs/01.jpg" alt="" /></a>
+								<h3>Colectivo Lateral de Arquitectura</h3>
+								<p>Lorem ipsum dolor sit amet nisl sed nullam feugiat.</p>
+							</article>
+							<article class="col-6 col-12-xsmall work-item">
+								<a href="images/fulls/02.jpg" class="image fit thumb"><img src="<?php echo get_stylesheet_directory_uri()?>/images/thumbs/02.jpg" alt="" /></a>
+								<h3>5 Elementos</h3>
+								<p>Lorem ipsum dolor sit amet nisl sed nullam feugiat.</p>
+							</article>
+							<article class="col-6 col-12-xsmall work-item">
+								<a href="images/fulls/03.jpg" class="image fit thumb"><img src="<?php echo get_stylesheet_directory_uri()?>/images/thumbs/03.jpg" alt="" /></a>
+								<h3>Webb App (MERNG)</h3>
+								<p>Lorem ipsum dolor sit amet nisl sed nullam feugiat.</p>
+							</article>
+							<article class="col-6 col-12-xsmall work-item">
+								<a href="images/fulls/04.jpg" class="image fit thumb"><img src="<?php echo get_stylesheet_directory_uri()?>/images/thumbs/04.jpg" alt="" /></a>
+								<h3>Tetris App</h3>
+								<p>Lorem ipsum dolor sit amet nisl sed nullam feugiat.</p>
+							</article>
+							<article class="col-6 col-12-xsmall work-item">
+								<a href="images/fulls/05.jpg" class="image fit thumb"><img src="<?php echo get_stylesheet_directory_uri()?>/images/thumbs/05.jpg" alt="" /></a>
+								<h3>Flappy Bird</h3>
+								<p>Lorem ipsum dolor sit amet nisl sed nullam feugiat.</p>
+							</article>
+							<article class="col-6 col-12-xsmall work-item">
+								<a href="images/fulls/06.jpg" class="image fit thumb"><img src="<?php echo get_stylesheet_directory_uri()?>/images/thumbs/06.jpg" alt="" /></a>
+								<h3>Video Loader</h3>
+								<p>Lorem ipsum dolor sit amet nisl sed nullam feugiat.</p>
+							</article>
+							<article class="col-6 col-12-xsmall work-item">
+								<a href="images/fulls/07.jpg" class="image fit thumb"><img src="<?php echo get_stylesheet_directory_uri()?>/images/thumbs/07.jpg" alt="" /></a>
+								<h3>Raw Wordpress Portfolio</h3>
+								<p>Lorem ipsum dolor sit amet nisl sed nullam feugiat.</p>
+							</article>
+						</div>
+						<ul class="actions">
+							<li><a href="https://github.com/alfonsojk" class="button">full portfolio</a></li>
+						</ul>
+					</section>
+				<!-- Three -->
+					<section id="three">
+						<h2 style="text-decoration:overline;">get in touch</h2>
+						<p>let's talk<br/>wanna get in touch or talk about a project?<br/>feel free to contact me via email <a style="text-decoration:overline;" href="mailto: alentres@gmail.com"
+				>at_alenthres@gmail.com </a><br/>
+			<div style="
+    display:flex;justify-content:center;
+    margin-bottom: 10%;
+">or fill the form</div>
+						<div class="row"  style="
+    display:flex;justify-content:center;">
+							<div class="col-8 col-12-small">
+								<form method="post" action="#">
+									<div class="row gtr-uniform gtr-50">
+										<div class="col-6 col-12-xsmall"><input type="text" name="name" id="name" placeholder="Name" /></div>
+										<div class="col-6 col-12-xsmall"><input type="email" name="email" id="email" placeholder="Email" /></div>
+										<div class="col-12"><textarea name="message" id="message" placeholder="Message" rows="4"></textarea></div>
+									</div>
+								</form>
+								<ul class="actions">
+									<li><input type="submit" value="Send Message" /></li>
+								</ul>
+							</div>
+						</div>
+					</section>
+
+		<!-- Footer -->
+			<footer id="footer">
+				<div class="inner">
+					<ul class="icons">
+						<li><a href="#" class="icon brands fa-github"><span class="label">Github</span></a></li>
+						<li><a href="#" class="icon brands fa-instagram"><span class="label">Instagram</span></a></li>
+					</ul>
+				</div>
+			</footer>
+
+		<!-- Scripts -->
+			<script src="/assets/js/jquery.min.js"></script>
+			<script src="/assets/js/jquery.poptrox.min.js"></script>
+			<script src="/assets/js/browser.min.js"></script>
+			<script src="/assets/js/breakpoints.min.js"></script>
+			<script src="/assets/js/util.js"></script>
+			<script src="/assets/js/main.js"></script>
+
+	</body>
+</html>
+<?php get_footer(); ?>
